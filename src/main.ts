5395364cd9c5f7d63bc0b046614034a031c2dedc
fiskhandlarn <oskar@joelson.org>
2024-10-03 21:11:07 +0200

    feat: use `pixelArt`

--- src/main.ts
+++ src/main.ts
@@ -9,22 +9,23 @@ import { Game, Types } from "phaser";
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 532,
-    height: 418,
-    parent: 'game-container',
-    backgroundColor: '#000000',
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    scene: [
-        Boot,
-        Preloader,
-        MainMenu,
-        MainGame,
-        GameOver
-    ]
+  type: Phaser.AUTO,
+  width: 532,
+  height: 418,
+  parent: 'game-container',
+  backgroundColor: '#000000',
+  pixelArt: true,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: [
+    Boot,
+    Preloader,
+    MainMenu,
+    MainGame,
+    GameOver
+  ]
 };
 
 export default new Game(config);
