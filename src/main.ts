29371da16ac6a38617045907902ee899fdb68d80
fiskhandlarn <oskar@joelson.org>
2024-10-03 20:46:30 +0200

    feat: reinit phaser with webpack instead
    ```
    $ pnpm create @phaserjs/game@latest
    √ Enter Project Name: ... smekjrrtolkientillutlosning
    √ Select Option: » Web Bundler (Vite, Bun, Webpack, Remix, ESBuild, Parcel, Rollup or ImportMap)
    √ Select Template: » esbuild
    √ Minimal or Complete Project: » Complete (Common Phaser Scene selection)
    √ Select Development Language: » TypeScript
    ```

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
 
-import { Game, type Types } from "phaser";
+import { Game, Types } from "phaser";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
